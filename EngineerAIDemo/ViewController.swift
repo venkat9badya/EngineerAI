f87331b0d1ffde9413bb2b7f3b4e8367cad44723
Venkatesh Badya <venkatesh@cellpointmobile.com>
2020-03-02 20:36:08 +0530

    Initial Commit

---
+++ EngineerAIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EngineerAIDemo
+//
+//  Created by Venkatesh on 3/2/20.
+//  Copyright © 2020 EngineerAI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EngineerAIDemoTests/EngineerAIDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  EngineerAIDemoTests.swift
+//  EngineerAIDemoTests
+//
+//  Created by Venkatesh on 3/2/20.
+//  Copyright © 2020 EngineerAI. All rights reserved.
+//
+
+import XCTest
+@testable import EngineerAIDemo
+
+class EngineerAIDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
